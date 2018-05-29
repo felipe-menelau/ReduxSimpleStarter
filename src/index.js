90dbad29ae7dafff3f0c9763dadeb7f8c9b03534
Felipe Menelau <felipe.menelau@skyhub.com.br>
2018-05-29 15:22:51 -0300

    Search bar is now a class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = process.env.API_KEY
+import SearchBar from './components/search_bar';
 
+const API_KEY = process.env.API_KEY;
 
 const App = () => {
-  return <div>{API_KEY}</div>;
-}
+  return(
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
